8cebc499c78cc25e3104effd2c9230c4d5ac66cd
kirti <ki@mail.com>
2020-09-22 08:56:13 +0000

    Added a comment line for new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 //This program will print simple message
 class HelloWorld {
     public static void main(String[] args) {
+	// prints "Hello world"
         System.out.println("Hello, World!"); 
+	// "prints welcome to git"
 	System.out.println("Welcome to Git!");
     }
 }
